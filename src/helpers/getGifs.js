6edf9e79d5df80e382a8884f422f282c895d6b64
Marcos Bascones <marcosbascones1991@gmail.com>
2025-06-03 16:12:23 +0200

    Documentacion geGitf.js

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,10 +1,12 @@
 export const getGifs=async(category)=>{
     const url= `https://api.giphy.com/v1/gifs/search?api_key=dR6WId4CQqPapcf2iVdnFHBW0Wb2jXnT&q=${category}&limit=5`;
 
+    // Realiza la petición HTTP con fetch y espera la respuesta
     const resp = await fetch(url);
-
+    // Convierte la respuesta en formato JSON y extrae la propiedad "data"
     const {data} = await resp.json();
 
+    // Mapea el array de resultados "data" para quedarse solo con los datos necesarios:
     const gifs= data.map(img=>({
       id: img.id,
       title: img.title,
