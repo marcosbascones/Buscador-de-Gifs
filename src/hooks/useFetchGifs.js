1c5ef67061fc04963d8e2e7b56d95bde9c3b6ecf
Marcos Bascones <marcosbascones1991@gmail.com>
2025-06-04 10:38:42 +0200

    Desarrollo useFetchGifs

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -7,12 +7,15 @@ export const useFetchGifs = (category) => {
 
     //Hook useState: 'images' será el array de gifs que se mostrará en pantalla
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   //Función que toma las imágenes es async porque la llamada de la api demanda un tiempo hasta que manda todos los datos necesarios
   const getImages = async () => {
     const newImages = await getGifs(category);
     //Actualiza el array con las imagenes guardadas en newImages tras llamar a getGifs
     setImages(newImages);
+    //Al haber obtenido las nuevas imagenes dejamos de cargar
+    setIsLoading(false);
   }
 
 
@@ -21,9 +24,10 @@ export const useFetchGifs = (category) => {
     getImages();
   }, []) // El array vacío [] indica que solo se ejecuta al renderizar por primera vez
 
+  //Se usa una sintaxis abreviada es lo mismo que hacer images:images
   return {
-    images: images,
-    isLoading: true
+    images,
+    isLoading
   }
     
   
